1d8738d750cbaddc47a5903b33a5492f38a0b058
mkarpacka <209348@edu.p.lodz.pl>
2019-07-05 15:00:04 +0200

    commit to check if travis is working 18

--- src/main/java/com/example/comarch/ComarchApplication.java
+++ src/main/java/com/example/comarch/ComarchApplication.java
@@ -9,6 +9,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
 @EnableScheduling
+
 public class ComarchApplication {
 
 	public static void main(String[] args) {
