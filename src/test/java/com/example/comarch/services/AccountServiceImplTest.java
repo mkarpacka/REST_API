cc26d57bfb073b65e85857bc9b7b76dc49cadd87
mkarpacka <209348@edu.p.lodz.pl>
2019-07-08 12:00:30 +0200

    tests destroyed to chceck deploy

--- src/test/java/com/example/comarch/services/AccountServiceImplTest.java
+++ src/test/java/com/example/comarch/services/AccountServiceImplTest.java
@@ -53,8 +53,8 @@ public class AccountServiceImplTest {
     public void whenAccountCurrencyIsNullShouldNotChangeCurrencyInUpdatedAccount() throws AccountDoesNotExistException {
         when(accountRepository.findByNumber(accountNumber)).thenReturn(account);
         Account updatedAccount = accountService.updateAccount(accountNumber, new Account(accountNumber, money, null, owner));
-
-        Assert.assertEquals(account.getCurrency(), updatedAccount.getCurrency());
+//        updatedAccount.getCurrency()
+        Assert.assertEquals(account.getCurrency(), null);
     }
 
     @Test
