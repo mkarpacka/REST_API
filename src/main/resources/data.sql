4db44e98b2e1134ac21171a6913175ac80f6a806
mkarpacka <209348@edu.p.lodz.pl>
2019-07-24 15:22:11 +0200

    add sql mock data updated 2

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-insert into account(id, currency, money, number, owner, is_deleted) values (null, 0, 100, "72134569542685123695454785", "Kowalski", 0);
-insert into account(id, currency, money, number, owner, is_deleted) values (null, 1, 200, "72134569542685123695454111", "Nowak", 0);
-insert into account(id, currency, money, number, owner, is_deleted) values (null, 1, 300, "72134569542685123695454222", "Fabicki", 0);
-insert into account(id, currency, money, number, owner, is_deleted) values (null, 0, 400, "72134569542685123695454333", "Szewc", 0);
-insert into account(id, currency, money, number, owner, is_deleted) values (null, 0, 500, "72134569542685123695454444", "Witczyk", 0);
+insert into account(id, currency, money, number, owner, is_deleted) values (null, 0, 100, '72134569542685123695454785', 'Kowalski', 0);
+insert into account(id, currency, money, number, owner, is_deleted) values (null, 1, 200, '72134569542685123695454111', 'Nowak', 0);
+insert into account(id, currency, money, number, owner, is_deleted) values (null, 1, 300, '72134569542685123695454222', 'Fabicki', 0);
+insert into account(id, currency, money, number, owner, is_deleted) values (null, 0, 400, '72134569542685123695454333', 'Szewc', 0);
+insert into account(id, currency, money, number, owner, is_deleted) values (null, 0, 500, '72134569542685123695454444', 'Witczyk', 0);
