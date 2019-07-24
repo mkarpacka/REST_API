ada23ccc7f9f1956e0b47e3525520f89359cd991
mkarpacka <209348@edu.p.lodz.pl>
2019-07-24 15:17:32 +0200

    add sql mock data updated

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO ACCOUNT (number, owner) values ("72163426348249393746456252", "Nowak")
-INSERT INTO ACCOUNT (number, owner) values ("72163426348249373737838390", "Kowalski")
-INSERT INTO ACCOUNT (number, owner) values ("72155555548249393746456252", "Szewc")
-INSERT INTO ACCOUNT (number, owner) values ("72163426348249393746499952", "Fabicki")
-INSERT INTO ACCOUNT (number, owner) values ("72163426348249333746499952", "Witczyk")
+insert into account(id, currency, money, number, owner, is_deleted) values (null, 0, 100, "72134569542685123695454785", "Kowalski", 0);
+insert into account(id, currency, money, number, owner, is_deleted) values (null, 1, 200, "72134569542685123695454111", "Nowak", 0);
+insert into account(id, currency, money, number, owner, is_deleted) values (null, 1, 300, "72134569542685123695454222", "Fabicki", 0);
+insert into account(id, currency, money, number, owner, is_deleted) values (null, 0, 400, "72134569542685123695454333", "Szewc", 0);
+insert into account(id, currency, money, number, owner, is_deleted) values (null, 0, 500, "72134569542685123695454444", "Witczyk", 0);
